29a0e14791e9c1489669682d1e98c0b551ffe2c6
Rui Freitas <a72399@alunos.uminho.pt>
2016-03-21 09:48:43 +0000

    engine novo

--- CG-2016/CG-2016/engine.cpp
+++ CG-2016/CG-2016/engine.cpp
@@ -189,6 +189,92 @@ int readFile3d(string filemodelo, char delem) {
 	return nVertices;
 }
 
+
+void readTranslate(TiXmlElement* elem) {
+	TiXmlElement* elem1;
+	const char* teste;
+
+	if ((elem1 = elem->FirstChildElement("translate")) != NULL) {
+
+		cout << "translate\n";
+		if (teste = elem1->Attribute("X")) {
+			cout << "X=" << teste << "\n";
+		}
+
+		if (teste = elem1->Attribute("Y")) {
+			cout << "Y=" << teste << "\n";
+		}
+		if (teste = elem1->Attribute("Z")) {
+			cout << "Z=" << teste << "\n";
+		}
+		cout << "endTranslate\n";
+	}
+}
+
+void readRotate(TiXmlElement* elem) {
+	TiXmlElement* elem1;
+	const char* teste;
+
+	//<rotate angle="45" axisX="0" axisY="1" axisZ="0" />
+
+	if ((elem1 = elem->FirstChildElement("rotate")) != NULL) {
+
+		if (teste = elem1->Attribute("angle")) {
+			cout << "angle=" << teste << "\n";
+		}
+
+		if (teste = elem1->Attribute("axisX")) {
+			cout << "axisX=" << teste << "\n";
+		}
+		if (teste = elem1->Attribute("axisY")) {
+			cout << "axisY=" << teste << "\n";
+		}
+		if (teste = elem1->Attribute("axisZ")) {
+			cout << "axisZ=" << teste << "\n";
+		}
+	}
+
+}
+
+void loadElementChild(TiXmlElement* elem) {
+
+	readTranslate(elem);
+	readRotate(elem);
+
+	for (elem = elem->FirstChildElement("group"); elem != NULL; elem = elem->NextSiblingElement()) {
+
+		loadElementChild(elem);
+
+	}
+}
+
+void readFileXML(char* file) {
+
+	TiXmlDocument doc;
+	const char* file;
+
+	if (doc.LoadFile(file))
+	{
+		try
+		{
+			for (TiXmlElement* elem = doc.FirstChildElement("scene")->FirstChildElement("group"); elem != NULL; elem = elem->NextSiblingElement()) {
+
+				loadElementChild(elem);
+
+			}
+		}
+		catch (const std::exception&)
+		{
+			cout << file << "\n";
+			printf("Failed to load file \"%s\"\n", file);
+		}
+	}
+	else
+	{
+		printf("Failed to open file \"%s\"\n", pFilename);
+	}
+}
+
 int main(int argc, char **argv) {
 	angle = 0;
 	betaCam = 0;
@@ -200,6 +286,7 @@ int main(int argc, char **argv) {
 	char* pFilename = argv[1];
 	TiXmlDocument doc;
 	const char* file;
+	
 	if (doc.LoadFile(pFilename))
 	{
 		try
