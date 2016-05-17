0cc5eea29511db6788df95c231588f5038801be5
jms05 <joaomsilva@outlook.com>
2016-05-17 13:27:46 +0100

    gerador desta fase

---
+++ JMS/Patch.h
@@ -0,0 +1,28 @@
+#ifndef PATCH_H
+#define PATCH_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class Patch
+{
+private: vector<int> v;
+
+public:
+	Patch();
+	Patch(vector<int> vi);
+	Patch(string s);
+
+	vector<int> getVector();
+
+	void addControl(int point);
+	void setVector(vector<int> vi);
+	int getAt(int i);
+};
+
+#endif
